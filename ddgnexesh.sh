8d1d4289e12ffa44bedfccc67635888ed2998f4c
ArtEze <passymas@gmail.com>
2021-01-22 11:13:25 +0000

    Update ddgnexesh.sh

--- ddgnexesh.sh
+++ ddgnexesh.sh
@@ -1,3 +1,4 @@
+ruta=$(dirname $0)
 res="./res.html"
 
 IFS="+"
@@ -7,4 +8,4 @@ echo "${argumentos}"
 curl -Ls --compressed --output $res "https://html.duckduckgo.com/html?q=${argumentos}" \
 -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:55.0) Gecko/20100101 Goanna/4.0 Firefox/55.0 Basilisk/20180202"
 
-./ddgnodesh $res
+${ruta}/ddgnodesh $res
